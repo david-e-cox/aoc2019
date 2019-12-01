7a4355d863f4951c542a599221feb84474254afd
Dave Cox <dave@harvestacuity.com>
2019-12-01 13:12:20 -0500

    Day 01 Complete

--- day01/d01.py
+++ day01/d01.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
-
 import numpy as np
+
+# Read input file
+f=open('input.txt');
+inval =[int(val) for val in f.read().split()];
+f.close()
+
+# Calculate fuel required
+totalFuel = int(0);
+partAFuel = int(0);
+for moduleMass in inval:
+    fuelIncrement = int(moduleMass/3) - 2;
+    partAFuel += fuelIncrement;
+    totalFuel += fuelIncrement;
+    while(fuelIncrement>0):
+        fuelIncrement = int(fuelIncrement/3) -2;
+        totalFuel += np.maximum(fuelIncrement,0);
+
+# Print solution
+print("Part A: Solution is {0:d}".format(partAFuel));
+print("Part B: Solution is {0:d}".format(totalFuel));
